69695b26f942db98e10656be0aea70035bb8fa49
thiagoN13 <thiago.nogueira187@gmail.com>
2019-08-30 17:29:07 -0300

    [NEW] Criado listagem das salas

---
+++ src/pages/lobby/salas.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="lobby">
+    <c-card-sala
+      v-for="(sala, index) in salas"
+      :key="index"
+      :sala="sala.nome"
+      :jogadores="sala.jogadores"
+      @click.native="btnComecar(sala)">
+    </c-card-sala>
+  </div>
+</template>
+
+<script>
+import cCardSala from '../../components/cards/c-card-sala'
+
+export default {
+  name: 'lobby',
+
+  components: {
+    cCardSala
+  },
+
+  data () {
+    return {
+      salas: [
+        { nome: 'Sala 1', jogadores: 10, jogo: 'snake', id: 1 }
+      ]
+    }
+  },
+
+  methods: {
+    btnComecar (sala) {
+      const { jogo, id } = sala
+      this.$router.push(`/lobby/${jogo}/${id}/nick`)
+    }
+  }
+}
+</script>
