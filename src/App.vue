44cd4bb9bb4954341e5911bc17f7c49cb1de351e
thiagoN13 <thiago.nogueira187@gmail.com>
2019-08-30 17:19:54 -0300

    [UPD] Adicionado fundo com cores

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,57 @@
 <template>
-  <div id="app">
-    <home/>
+  <div id="app" :style="{ 'background-color': corAtual }">
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import home from './components/home'
+import { obtainArrayOfInterpolatedColors, getRandomColor } from './lib/cores'
 
 export default {
   name: 'app',
-  components: {
-    home
+
+  data () {
+    return {
+      cores: [],
+      corAtual: null,
+      indexCor: 0
+    }
+  },
+
+  mounted () {
+    this.cores = obtainArrayOfInterpolatedColors(getRandomColor(), getRandomColor(), 10)
+    this.atualizarCor()
+
+    setInterval(this.atualizarCor, 5000)
+  },
+
+  methods: {
+    btnComecar () {
+      this.$router.push('/tabuleiro')
+    },
+
+    atualizarCor () {
+      this.corAtual = this.cores[this.indexCor]
+
+      this.indexCor++
+
+      if (this.indexCor === this.cores.length) {
+        const novaCor = getRandomColor()
+
+        this.cores = obtainArrayOfInterpolatedColors(novaCor, this.corAtual, 10)
+
+        this.indexCor = 0
+      }
+    }
   }
 }
 </script>
+
+<style>
+#lobby {
+  width: 100%;
+  height: 100%;
+  transition: 2s background-color ease
+}
+
+</style>
